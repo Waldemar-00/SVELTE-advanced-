085ffc41acebe6ed699c70d3927bd74261a84bae
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-04-27 15:06:20 +0300

    Used an attribute of input - multiple.

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -7,27 +7,35 @@
 		{ question: 'How old are you?', qst: 'Age: ' }
 	];
 	let answ;
-	let selected;
+	let selected = [];
 	let answers = [];
+	let disabled = true;
 	$: {
+		console.log(selected);
 		console.log(answers);
 	}
 	function addedAnswerHandler() {
-		answers = [...answers, { answ, qst: selected.qst }];
+		answers = [...answers, { answ, qst: selected }];
+		disabled = false;
 	}
 </script>
 
 <form on:submit|preventDefault={addedAnswerHandler}>
-	<select name="" id="" bind:value={selected}>
+	<select
+		name="selectedQuestions"
+		multiple
+		bind:value={selected}
+		on:change={() => (disabled = true)}
+	>
 		{#each options as option, index (index)}
-			<option value={option}>{option.question}</option>
+			<option value={option.qst}>{option.question}</option>
 		{:else}
 			<p>No options!</p>
 		{/each}
 	</select>
 	<hr />
 	<input type="text" bind:value={answ} />
-	<button disabled={!answ}>Add answer</button>
+	<button disabled={!(answ && selected.length > 0 && disabled)}>Add answer</button>
 </form>
 
 <ul>
