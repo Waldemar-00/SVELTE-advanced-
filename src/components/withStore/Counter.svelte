0c87f13c994500f90910120458686bd711437d3a
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-05-02 19:59:04 +0300

    Custom subscribe control.

--- src/components/withStore/Counter.svelte
+++ src/components/withStore/Counter.svelte
@@ -1,11 +1,20 @@
 <script>
+	// @ts-nocheck
+
 	import Increment from './Increment.svelte';
 	import Decrement from './Decrement.svelte';
 	import Reset from './Reset.svelte';
+	import { onDestroy } from 'svelte';
 	import { counter } from '$lib/store';
+	let changedCounter;
+	const unsubscribe = counter.subscribe((c) => {
+		changedCounter = c * 2;
+	});
+	onDestroy(unsubscribe);
 </script>
 
-<h1>{$counter}</h1>
+<!-- <h1>{$counter}</h1> -->
+<h1>{changedCounter}</h1>
 <Increment />
 <Decrement />
 <Reset />
