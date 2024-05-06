f6fd85d871168289fb4fb7ad83b0777903c63c15
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-05-06 20:51:24 +0300

    Renamed transition folder to motion folder.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,8 +42,8 @@
 	// import Time from '../components/withStore/Time.svelte';
 	// import InnerHTML from '../components/InnerHTML.svelte';
 	// import Action from '../components/actions/Action.svelte';
-	import Tweened from '../components/transitions/Tweened.svelte';
-	import Spring from '../components/transitions/Spring.svelte';
+	import Tweened from '../components/motion/Tweened.svelte';
+	import Spring from '../components/motion/Spring.svelte';
 </script>
 
 <h1>Advanced Svelte!</h1>
