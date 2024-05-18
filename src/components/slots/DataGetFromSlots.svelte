4716b82babaf085437305f320f6c91da7e24b21a
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-05-18 19:41:07 +0300

    The $$slots.property VS the if statement in the named slots.

--- src/components/slots/DataGetFromSlots.svelte
+++ src/components/slots/DataGetFromSlots.svelte
@@ -4,6 +4,21 @@
 
 <DataUpSlots let:co={color}>
 	<li style:color style:border-color={color}>{color}</li>
+	<p slot="article">
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis praesentium mollitia possimus,
+		ab dolore eum dignissimos eos quaerat cum? Fuga esse accusamus exercitationem ullam iste aliquid
+		harum ea nisi consequatur? Sapiente maxime aliquid laudantium est saepe voluptatem dignissimos
+		libero, sit reiciendis commodi, consectetur eligendi? Voluptatibus neque eveniet nostrum quae.
+		Facere ullam delectus, amet temporibus dignissimos eius nostrum odio velit enim? Dicta
+		doloremque, itaque, dolor ex saepe dolores sapiente quaerat porro cumque, nam ut quod in
+		doloribus. Fugit porro voluptatum est explicabo nesciunt. Nihil voluptates aut possimus odit in
+		esse quasi? Vel debitis laboriosam, aspernatur hic tempora dicta assumenda veniam ducimus
+		possimus recusandae beatae nulla ut. Incidunt perspiciatis omnis doloribus est eligendi sit
+		inventore magni illum, maiores, in qui, ab accusantium! Impedit totam quas, tempora non dicta
+		iste nesciunt, eius blanditiis fugiat laborum eos? Ipsum accusantium nisi at asperiores laborum
+		provident eum dolorem consequatur iure dicta dolore incidunt voluptas, aut laboriosam.
+	</p>
+	<h3 slot="parting">Have a good day!</h3>
 </DataUpSlots>
 
 <style>
@@ -14,5 +29,17 @@
 		border: 2px solid #eee;
 		margin-bottom: 0.5rem;
 		border-radius: 3.5vmin;
+	}
+	p {
+		border: 2px solid olivedrab;
+		padding: 1rem;
+		border-radius: 50px;
+		margin-top: 3rem;
+	}
+	h3 {
+		color: red;
+		margin: 0 auto;
+		width: 50vmin;
+		text-align: center;
 	}
 </style>
--- src/components/slots/DataUpSlots.svelte
+++ src/components/slots/DataUpSlots.svelte
@@ -1,6 +1,6 @@
 <script>
 	// @ts-nocheck
-
+	let parting = true;
 	let colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'grey'];
 	function filterColors(fn) {
 		colors = [...colors.sort(fn)];
@@ -18,6 +18,15 @@
 <button on:click={() => filterColors((a, b) => (a < b ? -1 : 1))}>Filter forward</button>
 <!-- <button on:click={filterColorsBack}>Filter back</button> -->
 <button on:click={() => filterColors((a, b) => (a < b ? 1 : -1))}>Filter back</button>
+<slot name="article">
+	<p>(article)</p>
+</slot>
+{#if parting && $$slots.parting}
+	<slot name="parting" />
+{:else}
+	<p>Good bye</p>
+{/if}
+<button on:click={() => (parting = !parting)}>Toggle parting</button>
 
 <style>
 	ul {
@@ -29,7 +38,7 @@
 	}
 	button {
 		display: block;
-		margin: 0 auto;
+		margin: 1rem auto;
 		width: 40vmin;
 		height: 4vmin;
 		padding: 0.4rem;
@@ -37,12 +46,18 @@
 		color: #fff;
 		border-radius: 2vmin;
 		border: none;
-		box-shadow: 3px 3px 3px 0 rgb(255, 198, 128);
+		box-shadow: 3px 3px 3px 0 rgb(255 140 0 / 0.7);
 		cursor: pointer;
 		margin-bottom: 0.7rem;
 	}
 	button:nth-child(2) {
 		background-color: red;
-		box-shadow: 3px 3px 3px 0 red;
+		box-shadow: 3px 3px 3px 0 rgb(255 0 0 / 0.7);
+	}
+	p {
+		margin: 2rem auto;
+		color: red;
+		width: 70vmin;
+		text-align: center;
 	}
 </style>
